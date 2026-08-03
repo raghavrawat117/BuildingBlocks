24f6b99101c9547ad693ee5e0e6758a2aca3240f
raghavrawat117 <raghav.rawat@capgemini.com>
2026-08-04 00:27:35 +0530

    Establishing DDD web API

---
+++ dev3/WebAPI/ddd-Employee-PhysicalPerson/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ddd_Employee_PhysicalPerson.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> GetWeatherForecast()
+    {
+        int a = 0;
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ dev3/WebAPI/ddd-Employee-PhysicalPerson/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace ddd_Employee_PhysicalPerson;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
